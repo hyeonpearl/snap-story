3945446d2e9effd63a8f860a9234024df6abae9d
hyeonpearl <hc9064@gmail.com>
2023-08-24 15:08:00 +0900

    chore: Firebase Setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import firebase from './_zlib/server/firebase';
 import App from './App';
+
+console.log(firebase);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
