19c7809a5a04b889d3f61eff7953a963f67cb10c
hyeonpearl <hc9064@gmail.com>
2023-08-25 13:40:56 +0900

    rename: firebase.js 이름 변경
    firebase => firebaseInstance

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import firebase from './_zlib/server/firebase';
 import App from './App';
-
-console.log(firebase);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
