ea066bc4ef422a1635f11a68a612c7a5b7bd9870
hyeonpearl <hc9064@gmail.com>
2023-09-13 14:56:17 +0900

    feat: PostTweet 컴포넌트 추가

---
+++ src/_zlib/hooks/usePostTweet.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+export default function usePostTweet() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [tweet, setTweet] = useState('');
+  const [file, setFile] = useState<File | null>(null);
+
+  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTweet(e.target.value);
+  };
+  const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = e.target;
+    if (files && files.length === 1) setFile(files[0]);
+  };
+
+  return { isLoading, tweet, file, onChange, onFileChange };
+}
