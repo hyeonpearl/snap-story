00bbe471b55af505535095974ac564e0ca0ea116
hyeonpearl <hc9064@gmail.com>
2023-09-19 16:18:34 +0900

    feat: Loading 컴포넌트 적용
    * Tweet 게시, 계정 만들기, 로그인 시 Loading 화면 출력

--- src/_zlib/components/PostTweet.tsx
+++ src/_zlib/components/PostTweet.tsx
@@ -5,6 +5,7 @@ import { Input } from './common/Input';
 import { Spacing } from './common/Spacing';
 import { Txt } from './common/Txt';
 import { Wrapper } from './common/Wrapper';
+import Loading from './Loading';
 import colors from '../constants/colors';
 import usePostTweet from '../hooks/usePostTweet';
 
@@ -14,35 +15,39 @@ export default function PostTweet() {
 
   return (
     <Wrapper className='post-tweet'>
-      <Form className='post-tweet' onSubmit={onSubmit}>
-        <Input.TextField
-          placeholder='무슨 일이 일어나고 있나요?'
-          value={tweet}
-          onChange={onChange}
-          rows={3}
-          maxLength={180}
-          required
-        />
+      {isLoading ? (
+        <Loading text='게시 중...' />
+      ) : (
+        <Form className='post-tweet' onSubmit={onSubmit}>
+          <Input.TextField
+            placeholder='무슨 일이 일어나고 있나요?'
+            value={tweet}
+            onChange={onChange}
+            rows={3}
+            maxLength={180}
+            required
+          />
 
-        <Wrapper className='buttons'>
-          <Wrapper className='row'>
-            <Input.Label htmlFor='file'>
-              <Icon.Photo color={colors.primary} />
-            </Input.Label>
-            <Input
-              id='file'
-              type='file'
-              accept='image/*'
-              onChange={onFileChange}
-            />
-            <Spacing direction={'horizontal'} size={10} />
-            {file && <Txt typography={'check'}>파일이 업로드되었습니다.</Txt>}
+          <Wrapper className='buttons'>
+            <Wrapper className='row'>
+              <Input.Label htmlFor='file'>
+                <Icon.Photo color={colors.primary} />
+              </Input.Label>
+              <Input
+                id='file'
+                type='file'
+                accept='image/*'
+                onChange={onFileChange}
+              />
+              <Spacing direction={'horizontal'} size={10} />
+              {file && <Txt typography={'check'}>파일이 업로드되었습니다.</Txt>}
+            </Wrapper>
+            <Button className='primary' type='submit'>
+              게시하기
+            </Button>
           </Wrapper>
-          <Button className='primary' type='submit'>
-            {isLoading ? '게시 중...' : '게시하기'}
-          </Button>
-        </Wrapper>
-      </Form>
+        </Form>
+      )}
     </Wrapper>
   );
 }
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,3 +1,4 @@
+import Loading from '../_zlib/components/Loading';
 import { Button } from '../_zlib/components/common/Button';
 import { Form } from '../_zlib/components/common/Form';
 import { Icon } from '../_zlib/components/common/Icon';
@@ -21,62 +22,66 @@ export default function SignUp() {
 
   return (
     <>
-      <Wrapper className='sign-form'>
-        <Icon src='/z.svg' width={250} height={250} />
-        <Txt typography={'h1'}>지금 일어나고 있는 일</Txt>
-        <Spacing direction='vertical' size={50} />
-        <Txt typography={'h4'}>지금 가입하세요.</Txt>
-        <Spacing direction='vertical' size={30} />
+      {isLoading ? (
+        <Loading text='계정 만드는 중...' />
+      ) : (
+        <Wrapper className='sign-form'>
+          <Icon src='/z.svg' width={250} height={250} />
+          <Txt typography={'h1'}>지금 일어나고 있는 일</Txt>
+          <Spacing direction='vertical' size={50} />
+          <Txt typography={'h4'}>지금 가입하세요.</Txt>
+          <Spacing direction='vertical' size={30} />
 
-        <Wrapper className='form'>
-          <Button className='social' onClick={onSignInGithub}>
-            <Icon src='github.svg' width={20} height={20} />
-            Github로 가입하기
-          </Button>
+          <Wrapper className='form'>
+            <Button className='social' onClick={onSignInGithub}>
+              <Icon src='github.svg' width={20} height={20} />
+              Github로 가입하기
+            </Button>
+          </Wrapper>
+
+          <Spacing direction='vertical' size={20} />
+          <LineBreak text='또는' />
+          <Spacing direction='vertical' size={20} />
+
+          <Form className='sign-form' onSubmit={onSubmit}>
+            <Input
+              name={'name'}
+              placeholder={'이름'}
+              value={form.name}
+              type={'text'}
+              onChange={onChange}
+              required
+            />
+            <Input
+              name={'email'}
+              placeholder={'이메일'}
+              value={form.email}
+              type={'email'}
+              onChange={onChange}
+              required
+            />
+            <Input
+              name={'password'}
+              placeholder={'비밀번호'}
+              value={form.password}
+              type={'password'}
+              onChange={onChange}
+              required
+            />
+            <Button className={'primary'} type={'submit'} required>
+              계정 만들기
+            </Button>
+            {error ? <Txt typography='error'>{error}</Txt> : null}
+            <Spacing direction={'vertical'} size={25} />
+
+            <Txt typography='h4'>이미 가입하셨나요?</Txt>
+            <Spacing direction='vertical' size={5} />
+            <Button className={'secondary'} onClick={moveToSignIn}>
+              로그인
+            </Button>
+          </Form>
         </Wrapper>
-
-        <Spacing direction='vertical' size={20} />
-        <LineBreak text='또는' />
-        <Spacing direction='vertical' size={20} />
-
-        <Form className='sign-form' onSubmit={onSubmit}>
-          <Input
-            name={'name'}
-            placeholder={'이름'}
-            value={form.name}
-            type={'text'}
-            onChange={onChange}
-            required
-          />
-          <Input
-            name={'email'}
-            placeholder={'이메일'}
-            value={form.email}
-            type={'email'}
-            onChange={onChange}
-            required
-          />
-          <Input
-            name={'password'}
-            placeholder={'비밀번호'}
-            value={form.password}
-            type={'password'}
-            onChange={onChange}
-            required
-          />
-          <Button className={'primary'} type={'submit'} required>
-            {isLoading ? '만드는 중...' : '계정 만들기'}
-          </Button>
-          {error ? <Txt typography='error'>{error}</Txt> : null}
-          <Spacing direction={'vertical'} size={25} />
-
-          <Txt typography='h4'>이미 가입하셨나요?</Txt>
-          <Spacing direction='vertical' size={5} />
-          <Button className={'secondary'} onClick={moveToSignIn}>
-            로그인
-          </Button>
-        </Form>
-      </Wrapper>
+      )}
     </>
   );
 }
