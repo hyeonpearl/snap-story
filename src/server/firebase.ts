eef8368c343c7c8d7d7d0e92b60b4a2317e33943
Hyeoncheol <hyeoncheol.contact@gmail.com>
2024-01-28 16:11:12 +0900

    fix: firebase 프로젝트 변경(api key 숨김)
    * firebase 프로젝트 변경(api key 숨김)

--- src/server/firebase.ts
+++ src/server/firebase.ts
@@ -6,15 +6,7 @@ import {
 } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyC6oKblvapCXVocLJBf2LPlqcM-YYy4O6g',
-  authDomain: 'zwitter-app.firebaseapp.com',
-  projectId: 'zwitter-app',
-  storageBucket: 'zwitter-app.appspot.com',
-  messagingSenderId: '496879371330',
-  appId: '1:496879371330:web:b44355af0e8d64de3b59db',
-};
+import { firebaseConfig } from './firebase.config';
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
